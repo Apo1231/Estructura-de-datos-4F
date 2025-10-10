998f952241a25bf7b14cb5beb7053cc6ef252b51
Francisco <20243ds155@utez.edu.mx>
2025-10-10 08:47:22 -0600

    terminado del 10 de octubre

---
+++ U1_Ti_03_SerieFib/src/test/Arreglos.java
@@ -0,0 +1,31 @@
+package test;
+
+public class Arreglos {
+    int[][] arrTest={
+            {1,3,7,8},
+            {0,6,3,1},
+            {1,2,3,4},
+            {0,5,3,0},
+            {2,5,9,0}
+    };
+
+    public Arreglos(int[][] arrTest, int resultado) {
+        this.arrTest = arrTest;
+        this.resultado = resultado;
+    }
+
+    int resultado;
+
+    public Arreglos() {
+
+    }
+
+    public void sumarArray(){
+        int largo = arrTest.length;
+        for (int [] i :arrTest){
+
+
+        }
+        System.out.println(resultado);
+    }
+}
---
+++ U1_Ti_03_SerieFib/src/test/Main.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Arreglos a = new Arreglos();
+
+        a.sumarArray();
+    }
+}
